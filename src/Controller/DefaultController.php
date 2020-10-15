343508245e37d750fe718d1d605f038dab70e121
biga156 <biga156@gmail.com>
2020-10-15 13:34:45 +0200

    reconfig

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\ImmoVente;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    
+    /** 
+     * @Route("/accueil", name="accueil")
+    */
+    public function accueil()
+    {
+        return $this->render('default/accueil.html.twig');
+    }
+    
+     /** 
+     * @Route("/enregistrements", name="enregistrements")
+    */
+    public function enregistrements()
+    {
+        return $this->render('default/accueil.html.twig');
+    }
+
+     /** 
+     * @Route("/user", name="user")
+    */
+    public function user()
+    {
+        return $this->render('default/accueil.html.twig');
+    }
+
+ /** 
+     * @Route("/autre", name="autre")
+    */
+    public function autre()
+    {
+        return $this->render('default/accueil.html.twig');
+    }
+
+/**
+     * 
+     * @Route("/index", name="index")
+     * 
+     */
+    public function index()
+    {
+        // Connexion à Doctrine,
+        // Connexion au Repository,
+        $repo = $this->getDoctrine()->getRepository(ImmoVente::class);
+        $immobiliers = $repo->findAll();
+
+        return $this->render('default/index.html.twig', [
+           'controller_name' => 'DefauultController',
+            // passage du contenu de $immobilier
+            'immobiliers'=>$immobiliers
+        ]);
+    }
+
+     /** 
+     * @Route("/test", name="test")
+    */
+    public function test()
+    {
+        return $this->render('default/test.html.twig');
+    }
+
+     /** 
+     * @Route("/administration", name="administration")
+    */
+    public function administration()
+    {
+        return $this->render('default/administration.html.twig');
+    }
+
+       /** 
+     * @Route("/connexion", name="connexion")
+    */
+    public function connexion()
+    {
+        return $this->render('default/connexion.html.twig');
+    }
+
+     /** 
+     * @Route("/nouscontacter", name="nouscontacter")
+    */
+    public function nouscontacter()
+    {
+        return $this->render('default/nouscontacter.html.twig');
+    }
+
+     /** 
+     * @Route("/les_ventes", name="les_ventes")
+    */
+    public function les_ventes()
+    {
+        return $this->render('default/les_ventes.html.twig');
+    }
+
+      /** 
+     * @Route("/les_locations", name="les_locations")
+    */
+    public function les_locations()
+    {
+        return $this->render('default/les_locations.html.twig');
+    }
+
+    /**
+     * @Route("/affichage", name="affichage")
+     * 
+     */
+	 
+	public function affichage()
+    {
+      return $this->render('default/affichage.html.twig');
+  }
+    
+    
+}
