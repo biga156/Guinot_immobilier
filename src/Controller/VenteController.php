677e1e65b123e515b9c8b960a93c20d74b8d550b
biga156 <biga156@gmail.com>
2020-10-20 16:41:27 +0200

    reorganisation2

---
+++ src/Controller/VenteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VenteController extends AbstractController
+{
+    /**
+     * @Route("/vente", name="vente")
+     */
+    public function index()
+    {
+        return $this->render('vente/index.html.twig', [
+            'controller_name' => 'VenteController',
+        ]);
+    }
+}
