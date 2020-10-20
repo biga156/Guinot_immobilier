b1597d3ac5ff1e99d7ba1de4358527a8249289cf
biga156 <biga156@gmail.com>
2020-10-20 16:04:54 +0200

    reorganisations

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,13 +2,13 @@
 
 namespace App\Controller;
 
-
 use App\Entity\ImmoVente;
 use App\Entity\Ventes;
 use App\Entity\Locations;
-use App\Form\VentesType;
 use App\Form\LocationsType;
+
 use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,64 +21,8 @@ class DefaultController extends AbstractController
     {
         $this->entityManager = $entityManager;
     }
-
+/****************************Forms************************** */
     /** 
-     * @Route("/accueil", name="accueil")
-    */
-    public function accueil()
-    {
-        return $this->render('default/accueil.html.twig');
-    }
-
-     /** 
-     * @Route("/immo/nouveau", name="immo.nouveau")
-    */   
-    public function nouveau(Request $request)
-    {
-        $entityManager = $this->entityManager;
-        $vente = new ImmoVente();
-
-        // Demande de al creation du Formaulaire avec CreateFormBuilder
-        $form = $this->createFormBuilder($vente)
-                    ->add('titre')
-                    ->add('photo')                
-                    ->add('description')    
-        //Utiser la Function GetForm pour voir le resultat Final
-                    ->getForm();
-        
-        // Traitement de la requete (http) passée en parametre
-        $form->handleRequest($request);
-
-        // Test sur le Remplissage / la soummision et la validité des champs
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-            // Affectation de la Date à mon article
-            $vente->setCreatedAt(new \DateTime());
-
-            $entityManager->persist($vente);
-            $entityManager->flush();
-
-            //Enregistrement et Retour sur la page de l'article
-            return $this->redirectToRoute('immo.nouveau', ['id'=>$vente->getId()]);
-        }
-         
-            
-        //aPassage à Twig des Variable à afficher avec lmethode CreateView
-        return $this->render('default/nouveau.html.twig', [
-            'formImmoVente' => $form->createView()
-        ]);
-    }
-    
-     /** 
-     * @Route("/enregistrements", name="enregistrements")
-    */
-    public function enregistrements()
-    {
-        return $this->render('default/enregistrements.html.twig');
-    }
-
-    
-     /** 
      * @param Request $request
      * @Route("/immo/vente", name="immo.vente")
      * @param Response
@@ -133,20 +77,6 @@ class DefaultController extends AbstractController
         ]);
     }
 
-     /**
-     * @Route("immo/{id}/delete", name="immo_delete", methods={"DELETE"})
-     */
-    
-    public function delete(Request $request, ImmoVente $immobilier): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$immobilier->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($immobilier);
-            $entityManager->flush();
-        }
-        return $this->redirectToRoute('index.accueil');
-    }
-
     
      /** 
      * @Route("/immo/location", name="immo.location")
@@ -156,7 +86,24 @@ class DefaultController extends AbstractController
         $entityManager = $this->entityManager;
         $location = new Locations();
 
-        $form = $this->createForm(LocationsType::class, $location); 
+        //$form = $this->createForm(LocationsType::class, $location); 
+        $form = $this->createFormBuilder($location)
+                ->add('createdAt')
+                ->add('titre')
+                ->add('categorie')
+                ->add('image')
+                ->add('description')
+                ->add('surface')
+                ->add('type_maison')
+                ->add('chambres')
+                ->add('etage')
+                ->add('cout')
+                ->add('adresse')
+                ->add('accesibilite')
+        //Utiser la Function GetForm pour voir le resultat Final
+                    ->getForm();
+                
+        
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
@@ -172,9 +119,27 @@ class DefaultController extends AbstractController
             'formLocation' => $form->createView()
         ]);
     }
-
-
-     /** 
+    
+/********************************End of forms*************************** */
+                    /**************Intermezzo************* */
+/********************************Actions************************ */
+   /**
+     * @Route("immo/{id}/delete", name="immo_delete", methods={"DELETE"})
+     */
+    
+    public function delete(Request $request, ImmoVente $immobilier): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$immobilier->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($immobilier);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('index.accueil');
+    }
+/******************************End of actions******************* */
+                /**************Intermezzo************* */
+/****************************Menus*************************** */
+/** 
      * @Route("/user", name="user")
     */
     public function user()
@@ -182,13 +147,7 @@ class DefaultController extends AbstractController
         return $this->render('default/user.html.twig');
     }
     
-     /** 
-     * @Route("/autre", name="autre")
-    */
-    public function autre()
-    {
-        return $this->render('default/autre.html.twig');
-    }
+   
 
  /**
      * 
@@ -325,4 +284,68 @@ class DefaultController extends AbstractController
         ]);
     }
     
+/***************************End of Menus********************* */
+            /**************Intermezzo************* */
+/********************** archived *******************************/
+   /* 
+     * @Route("/accueil", name="accueil")
+   *
+  
+    public function accueil()
+    {
+        return $this->render('default/accueil.html.twig');
+    }
+
+     
+    /* 
+     * @Route("/immo/nouveau", name="immo.nouveau")
+    
+    public function nouveau(Request $request)
+    {
+        $entityManager = $this->entityManager;
+        $vente = new ImmoVente();
+
+        // Demande de al creation du Formaulaire avec CreateFormBuilder
+        $form = $this->createFormBuilder($vente)
+                    ->add('titre')
+                    ->add('photo')                
+                    ->add('description')    
+        //Utiser la Function GetForm pour voir le resultat Final
+                    ->getForm();
+        
+        // Traitement de la requete (http) passée en parametre
+        $form->handleRequest($request);
+
+        // Test sur le Remplissage / la soummision et la validité des champs
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            // Affectation de la Date à mon article
+            $vente->setCreatedAt(new \DateTime());
+
+            $entityManager->persist($vente);
+            $entityManager->flush();
+
+            //Enregistrement et Retour sur la page de l'article
+            return $this->redirectToRoute('immo.nouveau', ['id'=>$vente->getId()]);
+        }
+         
+            
+        //aPassage à Twig des Variable à afficher avec lmethode CreateView
+        return $this->render('default/nouveau.html.twig', [
+            'formImmoVente' => $form->createView()
+        ]);
+    }
+
+    
+    /** 
+     * @Route("/enregistrements", name="enregistrements")
+    
+    public function enregistrements()
+    {
+        return $this->render('default/enregistrements.html.twig');
+    }
+    
+    */
+    /*******************End of archived********************** */
+  
 }
